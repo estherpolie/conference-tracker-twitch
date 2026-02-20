40f63d6671281a2e240cf3c1b77576daf007262d
estherpolie <polieesther6@gmail.com>
2026-02-20 10:13:58 +0200

    Refactor ConferenceController to improve code structure and readability

--- src/main/java/com/manning/sbip/twitch/conference_tracker_twitch/controller/ConferenceController.java
+++ src/main/java/com/manning/sbip/twitch/conference_tracker_twitch/controller/ConferenceController.java
@@ -18,75 +18,71 @@ import com.manning.sbip.twitch.conference_tracker_twitch.service.ConferenceServi
 
 import jakarta.validation.Valid;
 
-
 @Controller
 public class ConferenceController {
- private ConferenceService conferenceService;
- 
- @Autowired
- public ConferenceController(ConferenceService conferenceService) {
-	 this.conferenceService=conferenceService;
- }
- 
- @GetMapping
- public String index()
- {
-	 return "redirect:/index";
-	 
- }
- 
- @GetMapping("/index")
- public String index(Model model)
- {
-	 List<Conference> conferenceList = (List<Conference>) conferenceService.getConferences();
-	 model.addAttribute("conferences", conferenceList.isEmpty()?Collections.EMPTY_LIST: conferenceList);
-	 return "index";
-	 
- }
- @GetMapping("/addconference")
- public String showAddCourseForm(Conference course) {
-	 return "add-conference";
- }
- 
- @PostMapping("/addconference")
- public String addCourse(@Valid Conference course , BindingResult result , Model model ) {
-	 if(result.hasErrors()) {
-		 return "add:conference";
-			 
-	 }
-	 conferenceService.createConference(course);
-	 model.addAttribute("Conferences", conferenceService.getConferences());
-	 
-	return "redirect:/index";
- }
- @GetMapping("/update/{id}")
- public String showUpdateCourseForm(@PathVariable("id") Long id ,Model model) {
-	 model.addAttribute("conference",conferenceService.getConferenceById(id).get());
- 
-   return "update-conference";
- }
+	private ConferenceService conferenceService;
 
- @PutMapping("/update/{id}")
- public String updateCourse(@PathVariable("id") Long id,@Valid Conference conference , BindingResult result ,Model model) {
- if(result.hasErrors()) {
-	 conference.setId(id);
-	 return "update-conference";
-			 
- }
- conferenceService.updateConference(id, conference);
- model.addAttribute("conferences",conferenceService.getConferences());
- return "redirect:/index";
- }
- @DeleteMapping("/delete/{id}")
- public String deleteCourse(@PathVariable("id") Long id, Model model)
- {
-	 conferenceService.deleteConference(id);
-	 model.addAttribute("conferences",conferenceService.getConferences());
-	 return "redirect:/index";
- }
- 
+	@Autowired
+	public ConferenceController(ConferenceService conferenceService) {
+		this.conferenceService = conferenceService;
+	}
+
+	@GetMapping
+	public String index() {
+		return "redirect:/login";
+
+	}
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		List<Conference> conferenceList = (List<Conference>) conferenceService.getConferences();
+		model.addAttribute("conferences", conferenceList.isEmpty() ? Collections.EMPTY_LIST : conferenceList);
+		return "index";
+
+	}
+
+	@GetMapping("/addconference")
+	public String showAddCourseForm(Conference course) {
+		return "add-conference";
+	}
+
+	@PostMapping("/addconference")
+	public String addCourse(@Valid Conference course, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "add:conference";
+
+		}
+		conferenceService.createConference(course);
+		model.addAttribute("Conferences", conferenceService.getConferences());
+
+		return "redirect:/index";
+	}
+
+	@GetMapping("/update/{id}")
+	public String showUpdateCourseForm(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("conference", conferenceService.getConferenceById(id).get());
+
+		return "update-conference";
+	}
+
+	@PutMapping("/update/{id}")
+	public String updateCourse(@PathVariable("id") Long id, @Valid Conference conference, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			conference.setId(id);
+			return "update-conference";
+
+		}
+		conferenceService.updateConference(id, conference);
+		model.addAttribute("conferences", conferenceService.getConferences());
+		return "redirect:/index";
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteCourse(@PathVariable("id") Long id, Model model) {
+		conferenceService.deleteConference(id);
+		model.addAttribute("conferences", conferenceService.getConferences());
+		return "redirect:/index";
+	}
+
 }
-
- 
- 
-
